253496e506cf1cc5cc425317a9293169ab8b6c2c
esdras-santos <esantoz@protonmail.com>
2024-02-09 14:42:14 -0300

    deleted

--- contracts/lib/PerpSafeCast.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.7.6;
-
-library PerpSafeCast {
-    function toUint256(int128 num) internal pure returns (uint256) {
-        require(num >= 0, "SafeCast: value must be positive");
-        return uint256(num);
-    }
-}
